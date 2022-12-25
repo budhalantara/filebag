1211b1dba1dc1f75adf1567de1d7d7af2b00577b
budhalantara <budhalantara@gmail.com>
2022-12-25 21:15:52 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"reflect"
+	"sync"
+	"time"
+
+	"github.com/vbauerster/mpb/v8"
+	"github.com/vbauerster/mpb/v8/decor"
+)
+
+type Connection struct {
+	Index                uint
+	StartByte            uint64
+	EndByte              uint64
+	ByteLength           uint64
+	DownloadedBytesCount chan uint64
+	TmpFileName          string
+}
+
+func setupDir() (string, string) {
+	tmpPath := "./tmp"
+	resultPath := "./result"
+
+	if _, err := os.Stat(tmpPath); os.IsNotExist(err) {
+		if err := os.Mkdir("./tmp", 0744); err != nil {
+			log.Fatal("Failed to create tmp dir", err)
+		}
+	}
+
+	if _, err := os.Stat(resultPath); os.IsNotExist(err) {
+		if err := os.Mkdir("./result", 0744); err != nil {
+			log.Fatal("Failed to create result dir", err)
+		}
+	}
+
+	return tmpPath, resultPath
+}
+
+func main() {
+	if len(os.Args) <= 1 {
+		log.Fatal("1 argument required")
+	}
+
+	url := os.Args[1]
+	if url == "" {
+		log.Fatal("url is required")
+	}
+
+	tmpPath, resultPath := setupDir()
+
+	maxConnection := uint(4)
+
+	fm, err := GetFileMetadata(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !fm.AcceptRanges {
+		maxConnection = 1
+	}
+
+	connections := []Connection{}
+	cases := []reflect.SelectCase{}
+	progressBars := []*mpb.Bar{}
+	byteLenPerConn := fm.ContentLength / uint64(maxConnection)
+	contentSizeRemaining := fm.ContentLength
+
+	var wg sync.WaitGroup
+	p := mpb.New(mpb.WithWidth(40))
+
+	for i := uint(0); i < maxConnection; i++ {
+		startByte := uint64(i) * byteLenPerConn
+		endByte := startByte + byteLenPerConn - 1
+		if i == maxConnection-1 {
+			endByte = startByte + contentSizeRemaining
+		}
+
+		contentSizeRemaining -= byteLenPerConn
+
+		connection := Connection{
+			Index:                i,
+			StartByte:            startByte,
+			EndByte:              endByte,
+			ByteLength:           endByte - startByte + 1,
+			DownloadedBytesCount: make(chan uint64),
+			TmpFileName:          GenerateTmpFileName(tmpPath),
+		}
+
+		connections = append(connections, connection)
+		cases = append(cases, reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(connection.DownloadedBytesCount)})
+
+		wg.Add(1)
+		go func(i uint) {
+			defer wg.Done()
+
+			if err := Download(url, &connections[i]); err != nil {
+				log.Fatal(err)
+			}
+		}(i)
+
+		name := fmt.Sprintf("#%d", i+1)
+		bar := p.New(int64(connections[i].ByteLength),
+			mpb.BarStyle().Lbound("|").Filler("█").Tip("").Padding("░").Rbound("|"),
+			mpb.PrependDecorators(
+				decor.Name(name),
+			),
+			mpb.AppendDecorators(
+				decor.AverageSpeed(decor.UnitKiB, "% .2f"),
+				decor.Name(" | "),
+				decor.OnComplete(
+					decor.AverageETA(decor.ET_STYLE_GO),
+					"done",
+				),
+				decor.Name(" | "),
+				decor.Percentage(decor.WC{W: 4}),
+			),
+		)
+
+		progressBars = append(progressBars, bar)
+	}
+
+	wg.Wait()
+
+	HandleGracefulExit(connections)
+
+	remaining := len(cases)
+
+	for remaining > 0 {
+		chosen, val, ok := reflect.Select(cases)
+		if !ok {
+			// The chosen channel has been closed, so zero out the channel to disable the case
+			cases[chosen].Chan = reflect.ValueOf(nil)
+			remaining -= 1
+			continue
+		}
+
+		bar := progressBars[chosen]
+		bar.SetCurrent(int64(val.Uint()))
+	}
+
+	time.Sleep(1 * time.Second)
+
+	fOut, err := os.Create(filepath.Join(resultPath, fm.FileName))
+	if err != nil {
+		log.Fatal("Failed to create file: ", err)
+	}
+	defer fOut.Close()
+
+	// merge file & cleanup tmp
+	for _, conn := range connections {
+		fIn, err := os.Open(conn.TmpFileName)
+		if err != nil {
+			log.Fatal("Failed to open tmp file: ", err)
+		}
+		defer fIn.Close()
+
+		buf := make([]byte, 1024*1024) // 1 MiB
+		for {
+			n, err := fIn.Read(buf)
+			if err != nil && err != io.EOF {
+				log.Fatalf("Failed to read file: %s\nError: %s", conn.TmpFileName, err.Error())
+			}
+
+			if n == 0 {
+				break
+			}
+
+			fOut.Write(buf[:n])
+		}
+
+		os.Remove(conn.TmpFileName)
+	}
+}
---
+++ utils.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+)
+
+type FileMetadata struct {
+	ContentLength uint64
+	AcceptRanges  bool
+	FileName      string
+}
+
+func GetFileMetadata(url string) (res FileMetadata, err error) {
+	resp, err := http.Head(url)
+	if err != nil {
+		return res, err
+	}
+
+	headerContentLength := resp.Header.Get("Content-Length")
+	res.ContentLength, err = strconv.ParseUint(headerContentLength, 10, 0)
+	if err != nil {
+		return res, err
+	}
+
+	headerAcceptRanges := resp.Header.Get("Accept-Ranges")
+	res.AcceptRanges = headerAcceptRanges == "bytes"
+
+	urlParts := strings.Split(url, "/")
+	res.FileName = urlParts[len(urlParts)-1]
+
+	headerContentDisposition := resp.Header.Get("Content-Disposition")
+	if strings.HasPrefix(headerContentDisposition, "attachment") {
+		r, err := regexp.Compile(`filename.+"(.+)"`)
+		if err != nil {
+			return res, nil
+		}
+
+		res.FileName = r.FindStringSubmatch(headerContentDisposition)[1]
+	}
+
+	return res, err
+}
+
+func Download(url string, connection *Connection) (err error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("Error to create HTTP request: ", err.Error())
+	}
+
+	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", connection.StartByte, connection.EndByte))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error to do HTTP request: ", err.Error())
+	}
+
+	f, err := os.Create(connection.TmpFileName)
+	if err != nil {
+		log.Fatal("Failed to create file: ", err)
+	}
+
+	currentDownloaded := uint64(0)
+
+	go func() {
+		defer f.Close()
+		defer close(connection.DownloadedBytesCount)
+
+		buf := make([]byte, 1024*1024) // 1 MiB
+
+		for {
+			n, err := resp.Body.Read(buf)
+			if err != nil && err != io.EOF {
+				log.Fatal("Error reading HTTP response: ", err.Error())
+			}
+
+			if n == 0 {
+				break
+			}
+
+			currentDownloaded += uint64(n)
+			connection.DownloadedBytesCount <- currentDownloaded
+			f.Write(buf[:n])
+		}
+	}()
+
+	return nil
+}
+
+func HandleGracefulExit(connections []Connection) {
+	c := make(chan os.Signal)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	go func() {
+		<-c
+		log.Println("cleaning up tmp")
+		for _, conn := range connections {
+			os.Remove(conn.TmpFileName)
+		}
+		os.Exit(1)
+	}()
+}
+
+func GenerateTmpFileName(prefix string) string {
+	ns := time.Now().UnixNano()
+	nsStr := []byte(fmt.Sprintf("%d", ns))
+
+	rndBytes := make([]byte, 4)
+	rand.Read(rndBytes)
+
+	hash := md5.Sum(append(nsStr, rndBytes...))
+	hashStr := hex.EncodeToString(hash[:])
+
+	return filepath.Join(prefix, hashStr)
+}
